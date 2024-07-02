443f7f58bc504fae631cfdff67b154d3f9497720
gpetrov <g.petrov@artsofte.ru>
2024-07-02 22:16:54 +0500

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'sp-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = 'spotify84';
+}
