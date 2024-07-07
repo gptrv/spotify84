704e4b80fb3e7b3e4c64c72dc1c25119a88c297c
gpetrov <g.petrov@artsofte.ru>
2024-07-07 15:47:18 +0500

    t + e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
-import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
-import { TUI_SANITIZER, TuiAlertModule, TuiDialogModule, TuiRootModule } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'sp-root',
     standalone: true,
-    imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.less',
-    providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }]
+    styleUrl: './app.component.less'
 })
 export class AppComponent {
-    public title: string = 'spotify84';
+
 }
